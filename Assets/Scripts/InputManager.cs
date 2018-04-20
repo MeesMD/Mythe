381076e2461d588def54e41787a485e312208969
Koen Sanderse <koensanderse2000@gmail.com>
2018-04-20 12:19:43 +0200

    plz work

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public bool Up()
+    {
+        return Input.GetKeyDown("space");
+    }
+    public bool Left()
+    {
+        return Input.GetKey("a");
+    }
+    public bool Right()
+    {
+        return Input.GetKey("d");
+    }
+
+    public bool Pause()
+    {
+        return Input.GetKeyDown("p");
+    }
+}
